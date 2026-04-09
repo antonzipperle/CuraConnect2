9e953e4944ffc24e476ff0517f1cd07497241b7d
Anton <anton.zipperle@gmail.com>
2026-04-09 11:52:59 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: __dirname,
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': __dirname,
-    },
-  },
-  build: {
-    outDir: path.join(__dirname, 'dist'),
-    emptyOutDir: true,
-  },
   server: {
     hmr: process.env.DISABLE_HMR !== 'true',
     proxy: {
